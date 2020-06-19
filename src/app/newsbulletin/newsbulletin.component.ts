0b8ee4a59e38528996572ded2fccb7f204ed7d64
Sonam Gupta <songupta1@publicisgroupe.net>
2020-06-19 23:07:10 +0530

    changes for prev and next

--- src/app/newsbulletin/newsbulletin.component.ts
+++ src/app/newsbulletin/newsbulletin.component.ts
@@ -3,7 +3,7 @@ import { HackernewsService } from '../services/hackernews.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { switchMap, map } from 'rxjs/operators';
 import * as moment from 'moment';
-import {News, NewsItems} from '../News';
+import { News, NewsItems } from '../News';
 
 @Component({
   selector: 'app-newsbulletin',
@@ -17,72 +17,37 @@ export class NewsbulletinComponent implements OnInit {
   public totalPages: number;
   public feed: NewsItems[];
   private subscription: any;
-  
-  
+
+
   constructor(
-    private cdRef: ChangeDetectorRef,private route: ActivatedRoute,
+    private cdRef: ChangeDetectorRef, private route: ActivatedRoute,
     private router: Router,
-    private _api: HackernewsService){
+    private _api: HackernewsService) {
 
-    }
-
-  // ngOnInit(): void {
-  //   this.subscription = this.route.queryParams.
-  //   pipe(
-  //     switchMap(params => {
-  //       console.log(params.page);
-  //       this.page = +params.page || 1;
-  //       console.log(this.page);
-  //       return this._api.getFeed(this.page)
-  //     })
-  //   )
-  //   .pipe(
-  //     map((data) => {
-  //       console.log('time'+ data['hits'][0]);
-  //       data.hits.created_at = moment.unix(data.hits.created_at).fromNow();
-  //       Object.keys(data).map(k => 
-  //         {
-  //           console.log('data[k]' + data[k]);
-  //           data[k]
-  //         })
-  //       return data;
-  //     })
-  //   )
-  //   .subscribe(
-  //     (data) => {
-  //       console.log('subscribe' + data);
-  //       this.feed.push(data);
-  //     },
-  //     error => console.log(error)
-  //   );
-  // }
+  }
 
   ngOnInit() {
-    
-    // this._api.getFeed(1).subscribe((data: News[]) => {
-    //   this.feed = data['hits'];
-    //   console.log(this.feed);
-    // })
 
     this.subscription = this.route.queryParams.
-    pipe(
-      switchMap(params => {
-        console.log(params.page);
-        this.page = +params.page || 1;
-        console.log(this.page);
-        return this._api.getFeed(this.page)
+      pipe(
+        switchMap(params => {
+          console.log(params.page);
+          this.page = +params.page || 1;
+          console.log(this.page);
+          return this._api.getFeed(this.page)
+        })
+      ).
+      subscribe((data: News) => {
+        this.feed = (data.hits);
+        this.totalPages = +data.nbPages;
+        this.feed.forEach(function (f) {
+          f.isVisible = true;
+          f.created_at = moment.unix(new Date(f.created_at).getTime() / 1000).fromNow();
+          f.hostname = this.extractHostname(f.story_url);
+        }, this)
+        console.log(this.feed);
       })
-    ).
-    subscribe((data:News) => {
-      this.feed = (data.hits);
-      this.feed.forEach(function(f) {
-        f.isVisible = true;
-        f.created_at = moment.unix(new Date(f.created_at).getTime()/ 1000).fromNow();
-        f.hostname = this.extractHostname(f.story_url);
-      }, this)
-      console.log(this.feed);
-    })
-    
+
   }
 
   openUrl(url) {
@@ -103,6 +68,22 @@ export class NewsbulletinComponent implements OnInit {
 
   onHideClick(item) {
     item.isVisible = false;
+  }
+
+  onPrevClick() {
+    this.router.navigate(['/topStories'], {
+      queryParams: {
+        page: Math.max(1, this.page + 1)
+      }
+    });
+  }
+
+  onNextClick() {
+    this.router.navigate(['/topstories'], {
+      queryParams: {
+        page: Math.min(this.totalPages, this.page + 1)
+      }
+    });
   }
 
 }
